23ad830f915fd12d8e6bda569b877be8dbfe7e86
EthanLam1245 <74618941+EthanLam1245@users.noreply.github.com>
2021-01-24 20:34:51 -0500

    Update trivia.py

--- trivia.py
+++ trivia.py
@@ -1,7 +1,266 @@
 # Initializing trivia questions
 # Credit to https://thoughtcatalog.com/kelly-peacock/2020/04/multiple-choice-trivia-questions-and-answers/
 # and https://topessaywriter.org/multiple-choice-trivia-questions/ for TRIVIA QUESTIONS.
-TRIVIA_QUESTIONS = """Question: What is the name for the Jewish New Year?
+# Kennita. “The Best 250+ General Trivia Questions with Answers.” Opinion Stage, Opinion Stage, 31 May 2019, www.opinionstage.com/blog/trivia-questions/. Accessed 24 Jan. 2021.
+TRIVIA_QUESTIONS = """Question: Logarithm tables were invented by
+[A] John Napier
+[B] John Doe
+[C] John Harrison
+[D] John Douglas
+A
+
+Question: Name the instrument used to measure relative humidity
+[A] Hydrometer
+[B] Hygrometer
+[C] Barometer
+[D] Mercury Thermometer
+B
+
+Question: Marco Polo
+[A] discovered Greenland
+[B] travelled through China, India and other parts of Asia
+[C] travelled round the cape of Good Hope
+[D] discovered Canada
+B
+
+Question: Microphone is used to convert
+[A] sound waves into electrical energy
+[B] sound waves into light rays
+[C] electrical energy into sound waves
+[D] sound waves into magnetic currents
+A
+
+Question: Which among the following is also known as “Information Highway”?
+[A] Internet
+[B] Intranet
+[C] LAN
+[D] Web browser
+A
+
+Question: How many teeth are in tigers jaw”?
+[A] 14
+[B] 20
+[C] 30
+[D] 40
+C
+
+Question: Eyes of White Tigers are of which color?
+[A] Yellow
+[B] Blue
+[C] Brown
+[D] Black
+B
+
+Question: Which sense of tiger is most highly developed?
+[A] Taste
+[B] Smell
+[C] Sight
+[D] Hearing
+D
+
+Question: Choose the only vegetarian marine mammal
+[A] Manatee
+[B] Humback whale
+[C] Walrus
+[D] Sea Otter
+A
+
+Question: Which of the following mammals is a marsupial?
+[A] rabbit
+[B] kangaroo
+[C] elephant
+[D] human
+C
+
+Question: Stainless steel is so called because of its
+[A] High strength
+[B] High corrosion resistance
+[C] High ductility
+[D] Brittleness
+B
+
+Question: In white cast irons, carbon present as
+[A] Graphite flakes
+[B] Graphite nodules
+[C] Cementite
+[D] Carbon does not exist
+C
+
+Question: Refraction is caused by
+[A] different wave speeds
+[B] more than one reflection
+[C] displaced images
+[D] bending
+A
+
+Question: The smallest of the FORMED elements of the blood are the:
+[A] white cells
+[B] red cells
+[C] platelets
+[D] erythrocytes
+C
+
+Question: When looking at the cross section of the human tibia, one finds the
+RED marrow in the:
+[A] medullary cavity
+[B] cancellous bone
+[C] periosteum
+[D] epiphysis
+A
+
+Question: Cariology is the study of the:
+[A] human heart
+[B] tooth decay
+[C] kidneys
+[D] liver
+B
+
+Question: In cell division, the phase following the metaphas is known as:
+[A] prophase
+[B] anaphase
+[C] telophase
+[D] extophase
+B
+
+Question: During inspiration, the diaphragm moves:
+[A] down by contraction
+[B] down by relaxation
+[C] up by contraction
+[D] up by relaxation
+A
+
+Question: Digestion of carbohydrates begins where?
+[A] small intestines
+[B] colon
+[C] mouth
+[D] stomach
+C
+
+Question: Digestion of PROTEINS begins in which of the following human
+organs?
+[A] small intestines
+[B] colon
+[C] mouth
+[D] stomach
+D
+
+Question: A botanist is most likely to study:
+[A] Monerans
+[B] Protistans 
+[C] Fungi
+[D] Virions
+C
+
+Question: A virus must do what to reproduce? 
+[A] form a latent virus
+[B] undergo transformation
+[C] infect a cell
+[D] conjugate 
+C
+
+Question: A heart with a single atrium and single ventricle is a characteristic of
+adult:
+[A] amphibians
+[B] arthropods
+[C] birds
+[D] fish
+D
+
+Question: Which of the following is NOT a component of chromosomes?
+[A] histones
+[B] non-histone proteins
+[C] DNA
+[D] RNA
+D
+
+Question: Cell membranes are composed of:
+[A] proteins
+[B] plasmids
+[C] nucleopores
+[D] lysosomes
+A
+
+Question: Which of the following blood types is referred to as the universal
+RECIPIENT?
+[A] A
+[B] AB
+[C] O
+[D] B
+B
+
+Question: Approximately how many different types of cells make up the human
+body?
+[A] 200
+[B] 100
+[C] 80
+[D] 50
+A
+
+Question: An aggregate fruit is best represented by:
+[A] tomatoes
+[B] raspberries
+[C] plums
+[D] pears
+B
+
+Question: Spiders are members of what class?
+[A] Diplopoda
+[B] Insecta
+[C] Chilopoda
+[D] Arachnida
+D
+
+Question: What kind of acid is found in ants?
+[A] acetic
+[B] formic
+[C] nitric
+[D] glutamic
+B
+
+Question: Most of the carbon dioxide picked up by the blood in the tissues:
+[A] dissolves in the plasma
+[B] unites with hemoglobin
+[C] forms microscopic bubbles
+[D] diffuses into the alveoli
+A
+
+Question: Which of the following scientific terms is used to describe the loss of
+water vapor by plants. Is it:
+[A] digestion
+[B] respiration
+[C] adsorption
+[D] transpiration
+D
+
+Question: The tendency of a plant to grow toward light is called:
+[A] photosynthesis
+[B] photoinduction
+[C] phototropism
+[D] photolysis
+C
+
+Question: Peptide bonds are found in:
+[A] carbohydrates
+[B] lipids
+[C] nucleic acids
+[D] proteins
+D
+
+Question: Which of the following organs of the body produces insulin?
+[A] liver
+[B] stomach
+[C] pancreas
+[D] small intestine
+C
+
+Question: The major organ in mammals for detoxifying chemicals is:
+[A] kidney
+[B] liver
+[C] pancreas
+[D] stomach
+B
+
+Question: What is the name for the Jewish New Year?
 [A] Hanukkah
 [B] Yom Kippur
 [C] Kwanza
@@ -224,6 +483,335 @@ Question: How much of your vision do you lose if you go blind in one eye?
 [C] 35 percent
 [D] 20 percent
 D
+
+Question: Which car is often called the first muscle car?
+[A] Ferrari 488
+[B] Pontiac GTO
+[C] Ford Mustang
+[D] Toyota Rav4
+B
+
+Question: What does BMW stand for (in English)?
+[A] Bakian Motor Wheels
+[B] Bavarian Motor Works
+[C] Beamer Motor Works
+[D] Beau Mount Wagon
+B
+
+Question: Which company owns Bugatti, Lamborghini. Audi, Porsche, and Ducati?
+[A] Toyota
+[B] Hunda
+[C] Volkswagen
+[D] Chrysler
+C
+
+Question: Which natural disaster is measured with a Richter scale?
+[A] Earthquakes
+[B] Tornatos
+[C] Hurricane
+[D] Tsunami
+A
+
+Question: How many molecules of oxygen does ozone have?
+[A] 4
+[B] 2
+[C] 3
+[D] 1
+C
+
+Question: What animals are pearls found in?
+[A] Geoducks
+[B] Oyster
+[C] Clams
+[D] B & C
+B
+
+Question: What does “HTTP” stand for?
+[A] HyperText Transfer Protocol
+[B] Help Type The Page
+[C] HyperText Transfer Page
+[D] HyperText Terminal Pattern
+A
+
+Question: Which email service is owned by Microsoft?
+[A] Snail Mail
+[B] Yahoo Mail
+[C] Gmail
+[D] Hotmail
+D
+
+Question: Which Williams sister has won more Grand Slam titles?
+[A] Heather
+[B] Serena
+[C] Selina
+[D] Annie
+B
+
+Question: What sport was Jesse Owens involved in?
+[A] Basket ball
+[B] Bowling
+[C] Track and field
+[D] Hockey
+C
+
+Question: How many soccer players should each team have on the field at the start of each match?
+[A] 11
+[B] 9
+[C] 13
+[D] 10
+A
+
+Question: What is the common name for dried plums?
+[A] Dried plums
+[B] Prunes
+[C] Apples
+[D] Raisin
+B
+
+Question: What other name does “corn” go by?
+[A] Cob
+[B] Kernel
+[C] Corn dog
+[D] Maize
+D
+
+Question: What’s the primary ingredient in hummus?
+[A] Corn
+[B] Chickpeas
+[C] Beans
+[D] Wheat
+B
+
+Question: Which country invented tea?
+[A] China
+[B] India
+[C] Italy
+[D] France
+A
+
+Question: Which country produces the most coffee in the world?
+[A] India
+[B] Brazil
+[C] Egypt
+[D] Canada
+B
+
+Question: Which European nation was said to invent hot dogs?
+[A] Italy
+[B] Spain
+[C] Germany
+[D] Norway
+C
+
+Question: In which body part can you find the femur?
+[A] Leg
+[B] Arm
+[C] Foot
+[D] Hand
+A
+
+Question: Which bone are babies born without?
+[A] Crown
+[B] Tail bone
+[C] Knee cap
+[D] Adult teeth
+C
+
+Question: What is your body’s largest organ?
+[A] Heart
+[B] Lungs
+[C] Thigh
+[D] Skin
+D
+
+Question: What kind of cells are found in the brain?
+[A] Electrons
+[B] Neurons
+[C] Smart cells
+[D] Solar cells
+B
+
+Question: About how many taste buds does the average human tongue have?
+[A] 10,000
+[B] 100,000
+[C] 5,000
+[D] 50,000
+A
+
+Question: Which Avenger is the only one who could calm the Hulk down?
+[A] Spiderman
+[B] Black Widow
+[C] Ironman
+[D] Mrs. Hulk
+B
+
+Question: What is Hawkeye’s real name?
+[A] Tony Stark
+[B] Clark Kent
+[C] Clint Barton
+[D] James Simmons
+C
+
+Question: Night Crawler, member of the X-Men, has what kind of powers?
+[A] Shoot electricity
+[B] Can teleport
+[C] Can fly
+[D] Have harden skin
+B
+
+Question: What is the name of Batman’s butler?
+[A] Eric
+[B] Alex
+[C] Alfred
+[D] Carl
+C
+
+Question: What does DC stand for?
+[A] Draconian Cartoons
+[B] Detective Comics
+[C] Deadly Cosmos
+[D] Dangerous Community
+B
+
+Question: Which American state is the largest (by area)?
+[A] Alaska
+[B] Texas
+[C] California
+[D] Washington
+A
+
+Question: What is the smallest country in the world?
+[A] Egypt
+[B] Vatican City
+[C] Japan
+[D] Ireland
+B
+
+Question: Which continent is the largest?
+[A] Africa
+[B] North America
+[C] South America
+[D] Asia
+D
+
+Question: Which of the Seven Wonders is located in Egypt?
+[A] The Pyramids of Giza
+[B] Colossus of Rhodes
+[C] Lighthouse of Alexandria
+[D] The Temple of Artemis
+A
+
+Question: What is the capital of New Zealand?
+[A] Ottawa
+[B] Wellington
+[C] Sydney
+[D] Austria
+B
+
+Question: Which city in India would you find the Taj Mahal in?
+[A] Athens
+[B] Agra
+[C] Algeria
+[D] Argos
+B
+
+Question: What animal is on Levi’s logo?
+[A] Horse
+[B] Dog
+[C] Cat
+[D] Lion
+A
+
+Question: How many films did Sean Connery play James Bond in?
+[A] 4
+[B] 5
+[C] 6
+[D] 7
+D
+
+Question: What was the name of the actor who played Jack Dawson in Titanic?
+[A] Ben Affleck
+[B] Johnny Depp
+[C] Matt Damon
+[D] Leonardo DiCaprio
+D
+
+Question: How many Lord of the Rings films are there?
+[A] 2
+[B] 3
+[C] 4
+[D] 5
+B
+
+Question: Which popular TV show featured house Targaryen and Stark?
+[A] Walking Dead
+[B] Spartacus
+[C] Game of Thrones
+[D] Breaking Bad
+C
+
+Question: Which cartoon character lives in a pineapple under the sea?
+[A] Tom and Jerry
+[B] Spongebob Squarepants
+[C] Aquaman
+[D] Popeye The Sailor Man
+B
+
+Question: Which actor appeared in films “Face Off” and “Ghost Rider”?
+[A] Samuel L. Jackson
+[B] Hugh Jackman
+[C] Nicholas Cage
+[D] John Travolta
+C
+
+Question: Who played Wolverine?
+[A] John Travolta
+[B] Hugh Jackman
+[C] Nicole Kidman
+[D] Keanu Reeves
+B
+
+Question: Which of these songs was not by Michael Jackson?
+[A] Thriller
+[B] Beat It
+[C] Summer Night
+[D] Billie Jean
+C
+
+Question: How many eyes does a bee have?
+[A] 4
+[B] 5
+[C] 2
+[D] 8
+B
+
+Question: Which mammal has no vocal cords?
+[A] Giraffe
+[B] Elephant
+[C] Lizard
+[D] Zebra
+A
+
+Question: What name is used to refer to a group of frogs?
+[A] School
+[B] Gang
+[C] Herd
+[D] Army
+D
+
+Question: What was the name of the Egyptian God of the Sun?
+[A] Arturis
+[B] Ra
+[C] Zeus
+[D] Solaris
+B
+
+Question: Anubis, the God of Death, had the head of a …
+[A] Dragon
+[B] Jackal
+[C] Lion
+[D] Gargoyles
+B
 
 Question: What type of galaxy is the Milky Way galaxy?
 [A] Irregular
